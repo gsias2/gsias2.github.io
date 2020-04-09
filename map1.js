7972ac947739b26fff6b9fd34012217fef1a4020
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-09 16:27:10 -0500

    Update

--- map1.js
+++ map1.js
@@ -1,10 +1,10 @@
-var Map1 = L.map('Map1').setView([30.22, 93.21], 4)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(Map1)
-var Baton Rouge = L.marker([30.45, 91.18]).addTo(Map1)
+var map1 = L.map('map1').setView([30.22, 93.21], 4)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map1)
+var Baton Rouge = L.marker([30.45, 91.18]).addTo(map1)
 var Polygon = L.polygon([
   [31, -88],
   [35, -88],
   [32, -82]
-]).addTo(Map1)
+]).addTo(map1)
 renameThisPolygonObject.bindPopup('Polygon.')
 renameThisMarkerObject.bindPopup('Baton Rouge.')
