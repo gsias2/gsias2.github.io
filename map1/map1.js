17c12cb043b10dd2e2154257e1bba616030a18ea
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-13 14:30:46 -0500

    Update

--- map1/map1.js
+++ map1/map1.js
@@ -1,6 +1,6 @@
-var map1 = L.map('map1').setView([30.22, 93.21], 4)
+var map1 = L.map('map1').setView([32.18, -99.14], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map1)
-var renameThisMarkerObject = L.marker([30.45,91.18]).addTo(map1)
+var renameThisMarkerObject = L.marker([30, -90]).addTo(map1)
 var renameThisPolygonObject = L.polygon([
   [31, -88],
   [35, -88],
