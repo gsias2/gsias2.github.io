ebdf32e9badc428cd0e6df2073394520ff0bb29e
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-13 17:15:06 -0500

    Update

--- map2/map2.js
+++ map2/map2.js
@@ -1,5 +1,5 @@
-var map2 = L.map('map1').setView([32.18, -99.14], 4)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map2)
+var map2 = L.map('map2').setView([32.18, -99.14], 5)
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png').addTo(map2)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
