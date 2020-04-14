88ac604d67a1e6fa73bf9d6da35028ea536382c4
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-14 08:32:58 -0500

    Update

--- map3/map3.js
+++ map3/map3.js
@@ -1,3 +1,3 @@
 var map3 = L.map('map3').setView([32.18, -99.14], 5)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map3)
-var nolaswb 'https://opendata.arcgis.com/datasets/4d69d63b666547c1905977a3356cef3c_0.geojson'
+var nolaswb = 'https://opendata.arcgis.com/datasets/4d69d63b666547c1905977a3356cef3c_0.geojson'
