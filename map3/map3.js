5e16f6ad093a0de383780b01ab0d73aa3a6f163f
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-21 08:04:21 -0500

    Update

--- map3/map3.js
+++ map3/map3.js
@@ -2,5 +2,6 @@ var map3 = L.map('map3').setView([32.18, -99.14], 5)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map3)
 var statesurl1 = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(statesurl1, function (data) {
-  L.geoJSON(data,{ style: { color: 'green' } }).addTo(map3)
+  var statestyle = { color: 'green' }
+  L.geoJSON(data,{ style: statestyle }).addTo(map3)
 })
