07642f7c340f3c20fe49dae3cf68c387bbfb9453
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-24 16:19:12 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -5,10 +5,13 @@ var map4 = L.Wrld.map ('map4', 'e65117ae7fb552545c72547b0314b47d', {
 })
 map4.themes.setWeather(L.Wrld.themes.weather.Rainy)
 map4.themes.setTime(L.Wrld.themes.time.Day)
-var bikeshareUrlString = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
-jQuery.getJSON(bikeshareUrlString, function (data) {
+var bikestationUrlString = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
+jQuery.getJSON(bikestationUrlString, function (data) {
   L.geoJSON(data).addTo(map4)
 })
 L.geoJSON(data, {
   onEachFeature: renameThisCreatePopupFunction
 }).addTo(map4)
+var renameThisCreatePopupFunction = function (feature, layer) {
+  layer.bindPopup(feature.properties.NAME)
+}
