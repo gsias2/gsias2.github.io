e0839e3c35af005eee829c683931ab204b77176f
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-24 16:40:42 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -1,17 +1,11 @@
-var map4 = L.Wrld.map ('map4', 'e65117ae7fb552545c72547b0314b47d')
-var map4 = L.Wrld.map ('map4', 'e65117ae7fb552545c72547b0314b47d', {
+var map4 = L.Wrld.map('map', 'e65117ae7fb552545c72547b0314b47d')
+var map4 = L.Wrld.map('map', 'e65117ae7fb552545c72547b0314b47d', {
   center: [29.95, -90.07],
   zoom: 15
 })
-map4.themes.setWeather(L.Wrld.themes.weather.Rainy)
-map4.themes.setTime(L.Wrld.themes.time.Day)
-var bikestationUrlString = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
-jQuery.getJSON(bikestationUrlString, function (data) {
+var map4.themes.setWeather(L.Wrld.themes.weather.Rainy)
+var map4.themes.setTime(L.Wrld.themes.time.Day)
+var bikerstationurl = 'https://opendata.arcgis.com/datasets/63aa98f68bd54e528d7a75e6cf2492bd_0.geojson'
+jQuery.getJSON(bikerstationurl, function (data) {
   L.geoJSON(data).addTo(map4)
 })
-L.geoJSON(data, {
-  onEachFeature: renameThisCreatePopupFunction
-}).addTo(map4)
-var renameThisCreatePopupFunction = function (feature, layer) {
-  layer.bindPopup(feature.properties.NAME)
-}
