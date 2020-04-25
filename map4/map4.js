8a2df160352d55649b69392873e9dadb8e1491e0
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-24 20:21:00 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -12,7 +12,7 @@ var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d', {
    onEachFeature: renameThisCreatePopupFunction
  }).addTo(map4)
  var OrganizationName = function (feature, layer) {
-  layer.bindPopup(feature.properties.Name)
+  layer.bindPopup(feature.properties.NonProfitName)
  }
  L.geoJSON(data, {
    onEachFeature: renameThisCreatePopupFunction
