ea29695673a292327ab1ade70514ec6eaf65dcac
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-24 19:27:25 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -1,33 +1,32 @@
-var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d')
 var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d', {
   center: [29.95, -90.07],
   zoom: 15
 })
-var map4.themes.setWeather(L.Wrld.themes.weather.Rainy)
-var map4.themes.setTime(L.Wrld.themes.time.Day)
-var nonprofiturl = 'https://opendata.arcgis.com/datasets/ada75a6799874e09aa61d05ba65038e5_0.geojson'
-jQuery.getJSON(nonprofiturl, function (data) {
+ var map4.themes.setWeather(L.Wrld.themes.weather.Rainy)
+ var map4.themes.setTime(L.Wrld.themes.time.Day)
+ var nonprofiturl = 'https://opendata.arcgis.com/datasets/ada75a6799874e09aa61d05ba65038e5_0.geojson'
+ jQuery.getJSON(nonprofiturl, function (data) {
   L.geoJSON(data).addTo(map4)
 })
-L.geoJSON(data, {
-  onEachFeature: renameThisCreatePopupFunction
-}).addTo(map4)
-var Organization Name = function (feature, layer) {
+ L.geoJSON(data, {
+   onEachFeature: renameThisCreatePopupFunction
+ }).addTo(map4)
+ var Organization Name = function (feature, layer) {
   layer.bindPopup(feature.properties.Name)
-}
-var Organization Website = function (feature, layer) {
-  layer.bindPopup(feature.properties.Website)
-}
-jQuery('#Arts Council of New Orleans').on('click', function () {
-  map4.setView([29.952, -90.073], 17, {
+ }
+ var Organization Website = function (feature, layer) {
+   layer.bindPopup(feature.properties.Website)
+ }
+ jQuery('#Arts Council of New Orleans').on('click', function () {
+   map4.setView([29.952, -90.073], 17, {
     headingDegrees: -45,
     animate: true,
     durationSeconds: 3
-  })
-})
-jQuery('#Contemporary Arts Center').on('click', function () {
-  map4.setView([29.943, -90.070], 17, {
-    headingDegrees: -45,
+   })
+ })
+ jQuery('#Contemporary Arts Center').on('click', function () {
+   map4.setView([29.943, -90.070], 17, {
+     headingDegrees: -45,
     animate: true,
     durationSeconds: 3
   })
