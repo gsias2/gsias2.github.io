f6ff3168334156546d36db26cdb58967362717ed
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-24 19:19:25 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -1,5 +1,5 @@
-var map4 = L.Wrld.map('map','e65117ae7fb552545c72547b0314b47d')
-var map4 = L.Wrld.map('map','e65117ae7fb552545c72547b0314b47d', {
+var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d')
+var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d', {
   center: [29.95, -90.07],
   zoom: 15
 })
@@ -12,14 +12,21 @@ jQuery.getJSON(nonprofiturl, function (data) {
 L.geoJSON(data, {
   onEachFeature: renameThisCreatePopupFunction
 }).addTo(map4)
-var renameThisCreatePopupFunction = function (feature, layer) {
+var Organization Name = function (feature, layer) {
   layer.bindPopup(feature.properties.Name)
 }
-var renameThisCreatePopupFunction = function (feature, layer) {
+var Organization Website = function (feature, layer) {
   layer.bindPopup(feature.properties.Website)
 }
 jQuery('#Arts Council of New Orleans').on('click', function () {
   map4.setView([29.952, -90.073], 17, {
+    headingDegrees: -45,
+    animate: true,
+    durationSeconds: 3
+  })
+})
+jQuery('#Contemporary Arts Center').on('click', function () {
+  map4.setView([29.943, -90.070], 17, {
     headingDegrees: -45,
     animate: true,
     durationSeconds: 3
