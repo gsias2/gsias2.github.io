62dd74f5929817abb058ee9fbd7805ba6d503488
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-27 10:54:04 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -2,7 +2,7 @@ var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d', {
   center: [29.95, -90.07],
   zoom: 15
 })
- map4.themes.setWeather(L.Wrld.themes.weather.Rainy)
+ map4.themes.setWeather(L.Wrld.themes.weather.Clear)
  map4.themes.setTime(L.Wrld.themes.time.Dawn)
  var nonprofiturl1 = 'https://opendata.arcgis.com/datasets/ada75a6799874e09aa61d05ba65038e5_0.geojson'
  jQuery.getJSON(nonprofiturl1, function (data) {
