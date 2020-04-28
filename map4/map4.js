5aeb036e3ad00fa1bef09c917a84f1e56b0721ce
Gerren Sias <gerrensias@Gerrens-Air.attlocal.net>
2020-04-28 13:52:18 -0500

    Update

--- map4/map4.js
+++ map4/map4.js
@@ -9,18 +9,18 @@ var map4 = L.Wrld.map('map4','e65117ae7fb552545c72547b0314b47d', {
    L.geoJSON (data,{
      onEachFeature: createPopup
    }).addTo(map4)
- var createPopup = function (feature, layer) {
-  layer.bindPopup(feature.properties.NonProfitName + "<br>Website:" + feature.properties.Website)
- }
 })
- jQuery('#Arts Council of New Orleans').on('click', function () {
+var createPopup = function (feature, layer) {
+ layer.bindPopup(feature.properties.NonProfitName + "<br>Website:" + feature.properties.Website)
+}
+ jQuery('#artscouncilofneworleans').on('click', function () {
    map4.setView([29.952, -90.073], 17, {
     headingDegrees: -45,
     animate: true,
     durationSeconds: 3
    })
  })
- jQuery('#Contemporary Arts Center').on('click', function () {
+ jQuery('#contemporaryartscenter').on('click', function () {
    map4.setView([29.943, -90.070], 17, {
      headingDegrees: -45,
     animate: true,
